1e44718defb096d4c8fd0c6f83ce52afbb5bf5ce
dev-svpk <dev@solve.poker>
2025-10-29 21:15:03 +1100

    add feature rayon and impl par_iter for HandIter

--- openpql-prelude/src/card/hand_iter.rs
+++ openpql-prelude/src/card/hand_iter.rs
@@ -59,11 +59,46 @@ impl<const SD: bool, const N: usize> Iterator for HandIter<SD, N> {
 
         Some(HandN::new(cards))
     }
+
+    /// # Panics
+    /// May panic on 32-bit systems when the result exceeds `u32::MAX`.
+    /// For example, C(52, 26) ≈ 4.96 x 10¹⁴, which is greater than 2³² - 1 (4,294,967,295).
+    /// However, this function works correctly for typical small values of N, such as N = 7.
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let n = const { if SD { Card::N_CARDS_SD } else { Card::N_CARDS } };
+        let r = N;
+
+        let len = ncr(n as usize, r);
+
+        (len, Some(len))
+    }
+}
+
+impl<const SD: bool, const N: usize> ExactSizeIterator for HandIter<SD, N> {}
+
+pub fn ncr(n: usize, r: usize) -> usize {
+    if r > n {
+        return 0;
+    }
+
+    if r == 0 || r == n {
+        return 1;
+    }
+
+    let r = r.min(n - r);
+
+    let mut result: usize = 1;
+    for i in 0..r {
+        result = result * (n - i) / (i + 1);
+    }
+
+    result
 }
 
 #[cfg(test)]
 #[cfg_attr(coverage_nightly, coverage(off))]
 mod tests {
+    use super::*;
     use crate::*;
 
     fn handiter_vec<const N: usize, const SD: bool>() -> Vec<Vec<Card>> {
@@ -88,5 +123,24 @@ mod tests {
         const SD: bool = true;
         assert_eq!(handiter_vec::<2, SD>(), itertool_vec::<2, SD>());
         assert_eq!(handiter_vec::<3, SD>(), itertool_vec::<3, SD>());
+    }
+
+    #[quickcheck]
+    fn test_pascals_identity(n: usize, r: usize) -> TestResult {
+        if n == 0 || n > 52 || r == 0 || r > n {
+            return TestResult::discard();
+        }
+        // Pascal's identity C(n, r) = C(n-1, r-1) + C(n-1, r)
+        let lhs = ncr(n, r);
+        let rhs1 = ncr(n - 1, r - 1);
+        let rhs2 = ncr(n - 1, r);
+
+        TestResult::from_bool(lhs == rhs1 + rhs2)
+    }
+
+    #[test]
+    fn test_ncr() {
+        assert_eq!(ncr(52, 4), 270_725); // Omaha
+        assert_eq!(ncr(52, 5), 2_598_960);
     }
 }
